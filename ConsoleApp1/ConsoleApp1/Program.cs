b9bbb1e8110a37ce8235466bc3a943c218356198
lwenjim <779772852@qq.com>
2024-02-03 21:43:35 +0800

    feat: 1.command-line 2.http.get

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.CommandLine;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
@@ -18,31 +21,54 @@ namespace ConsoleApp1
     }
     internal class Program
     {
-        static void Main(string[] args)
+        static   async Task Main(string[] args)
         {
-            Account account = new Account
-            {
-                Email = "james@example.com",
-                Active = true,
-                CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
-                Roles = new List<string>
-                {
-                    "User",
-                    "Admin"
-                }
-            };
+            //Account account = new Account
+            //{
+            //    Email = "james@example.com",
+            //    Active = true,
+            //    CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
+            //    Roles = new List<string>
+            //    {
+            //        "User",
+            //        "Admin"
+            //    }
+            //};
+            //string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+            //Console.WriteLine(json);
 
-            string json = JsonConvert.SerializeObject(account, Formatting.Indented);
-            // {
-            //   "Email": "james@example.com",
-            //   "Active": true,
-            //   "CreatedDate": "2013-01-20T00:00:00Z",
-            //   "Roles": [
-            //     "User",
-            //     "Admin"
-            //   ]
-            // }
-            Console.WriteLine(json);
+
+            var rootCommand = new RootCommand
+            {
+                new Argument<string>("url","web site url"),
+                new Option<bool>(new string[]{ "--gethtml" ,"-html"},"Get html source"),
+                new Option<bool>(new string[]{ "--getimage" ,"-image"},"Get images"),
+                new Option<bool>(new string[]{ "--regex-option" ,"-regex"},"Use regex"),
+                new Option<bool>(new string[]{ "--htmlagilitypack-option", "-agpack"},"Use HtmlAgilityPack"),
+                new Option<bool>(new string[]{ "--anglesharp-option", "-agsharp"},"Use AngleSharp"),
+                new Option<string>(new string[]{ "--download-path" ,"-path"},"Designate download path"),            
+            };
+            var i =await rootCommand.InvokeAsync(args);
+            if (i != 0)
+            {
+                return;
+            }
+            Console.WriteLine(String.Format("输入的地址为：{0}", args[0]));
+
+            var uri = new Uri(args[0]);
+            var httpClient = new HttpClient();
+            try
+            {
+                var result = await httpClient.GetStringAsync(uri);
+                httpClient.Dispose();
+                Console.WriteLine(result);
+
+            }catch (Exception ex)
+            {
+                string msg = "Error "+ex.ToString();
+                httpClient.Dispose() ;
+                Console.WriteLine(msg);
+            }
         }
     }
 }
