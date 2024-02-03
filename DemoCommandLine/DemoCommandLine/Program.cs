15329dcb09fb38ed8a366a5a27e569c4f201a8db
lwenjim <779772852@qq.com>
2024-02-03 22:38:35 +0800

    add DemoCommondLine

---
+++ DemoCommandLine/DemoCommandLine/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.CommandLine;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DemoCommandLine
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // CTRL + R + G 清除无用包导入
+            var rootCommand = new RootCommand
+            {
+                new Argument<string>("url","web site url"),
+                new Option<bool>(new string[]{ "--gethtml" ,"-html"},"Get html source"),
+                new Option<bool>(new string[]{ "--getimage" ,"-image"},"Get images"),
+                new Option<bool>(new string[]{ "--regex-option" ,"-regex"},"Use regex"),
+                new Option<bool>(new string[]{ "--htmlagilitypack-option", "-agpack"},"Use HtmlAgilityPack"),
+                new Option<bool>(new string[]{ "--anglesharp-option", "-agsharp"},"Use AngleSharp"),
+                new Option<string>(new string[]{ "--download-path" ,"-path"},"Designate download path"),
+            };
+            var i = await rootCommand.InvokeAsync(args);
+            if (i != 0)
+            {
+                return;
+            }
+            Console.WriteLine(String.Format("正在获取: {0} 的内容", args[0]));
+
+            var uri = new Uri(args[0]);
+            var httpClient = new HttpClient();
+            try
+            {
+                var result = await httpClient.GetStringAsync(uri);
+                httpClient.Dispose();
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                string msg = "Error " + ex.ToString();
+                httpClient.Dispose();
+                Console.WriteLine(msg);
+            }
+        }
+    }
+}
---
+++ DemoJson/DemoJson/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoJson
+{
+    internal class Program
+    {
+        public class Account
+        {
+            public string Email { get; set; }
+            public bool Active { get; set; }
+            public DateTime CreatedDate { get; set; }
+            public IList<string> Roles { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Account account = new Account
+            {
+                Email = "james@example.com",
+                Active = true,
+                CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
+                Roles = new List<string>
+                {
+                    "User",
+                    "Admin"
+                }
+            };
+            string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+}
